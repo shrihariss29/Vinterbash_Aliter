a1c1e2b1d1938807ae1c1a781791e4aa99ae49d9
Shrihari <140818388+shrihariss29@users.noreply.github.com>
2024-06-02 11:10:12 +0530

    Update Home.js
    Add Carousel

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,3 +1,4 @@
+import Carousel from 'react-bootstrap/Carousel';
 
 function Home()
 {
@@ -10,8 +11,15 @@ function Home()
              <div id='homepage'>
              <h1 id='Vinter'>V I N T E R B A S H</h1>
              <h3 id='theme'>Nambikkai, naanayam, kairaasi</h3>
-             </div>
-         
+             </div>  
+      <Carousel className='carousel' interval={4000} wrap={true}>
+        {[1, 2, 3, 4,5,6,7].map((index) => (
+          <Carousel.Item key={index}>
+            <img className="carouselimg" src={require(`../assets/pic${index}.jpg`)} alt={`My pics in navbar ${index}`} loading="lazy" />
+            <Carousel.Caption></Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
             <div >
             <h2 className='hometitle'>About our event and Motto</h2>
             <p className='homeabout'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
@@ -21,6 +29,6 @@ function Home()
             Tristique et egestas quis ipsum suspendisse.Nisi lacus sed viverra tellus in hac habitasse platea dictumst. Eleifend quam adipiscing vitae proin.
             </p>
              </div>
-            </div>
+      </div>
 }
 export default Home;
